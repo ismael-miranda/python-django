1c1929556c51a5bad6b2a413062880e259b80626
laptop <ismael@ismael.com>
2021-06-30 22:53:24 -0300

    Criado app base close #3

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
 def home(request):
-    #retorna a mensagem
+    # retorna a mensagem
     return HttpResponse('Olá Mundo Django!')
