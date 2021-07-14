5a1c9eb6adbca67faf5d87ac5f7c9c849ce6cdc3
ismael <ismael@ismael>
2021-07-13 23:44:21 -0300

    Criado a Landing Page - Pagina inicial

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
     # retorna a mensagem
-    return HttpResponse('<html><body>Olá Mundo Django!</body></html>', content_type='text/html')
+    return render(request, 'base/home.html')
