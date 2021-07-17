784ca131f801f5f8e467b9cdf13518495ea04739
laptop <ismael@ismael.com>
2021-07-17 20:12:17 -0300

    Criado uma nova app. aperitivos, e adicionado o video servidor pelo vimeo

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from pypro.django_assertions import assert_contains
 
-# Create your tests here.
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp,
+                    '<iframe src="https://player.vimeo.com/video/576216807?badge=0&amp;autopause=0&amp;'
+                    'player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; '
+                    'picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;'
+                    'height:100%;" title="videoplayback"></iframe>')
