c14b553edef1437391be5457d83f0c522f9e2f7a
laptop <ismael@ismael.com>
2021-08-10 22:46:05 -0300

    Verificado o Pipfile, que nao atualizou na pasta correta.

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,24 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Video
 
 
-# class Video:
-#     def __init__(self, slug, titulo, vimeo_id):
-#         self.slug = slug
-#         self.titulo = titulo
-#         self.vimeo_id = vimeo_id
-#
-#     def get_absolute_url(self):
-#         return reverse('aperitivos:video', args=(self.slug,))
-
-
-# videos = [
-#     Video(slug="motivacao", titulo="Video Aperitivo: Motivação", vimeo_id='576216807'),
-#     Video(slug="instalacao-windows", titulo="Instalação Windows", vimeo_id='577835849'),
-# ]
-#
-# videos_dct = {v.slug: v for v in videos}
-
-
 def indice(request):
     videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={"videos": videos})
@@ -27,7 +9,5 @@ def indice(request):
 
 def video(request, slug):
     video = get_object_or_404(Video, slug=slug)
-
-    # video = videos_dct[slug]
 
     return render(request, 'aperitivos/video.html', context={"video": video})
